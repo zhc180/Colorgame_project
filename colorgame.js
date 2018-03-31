ae13c60e24da0d4000892ec6edcb076f6d2710ba
Zhongyu Chen <zhc180@ucsd.edu>
2018-03-31 01:55:55 -0500

    Added more difficulty level

--- colorgame.js
+++ colorgame.js
@@ -1,12 +1,14 @@
 var numSquares = 6;
 var colors = [];
 var pickedColor;
+var hardMode = false;
 var h1 = document.querySelector("h1");
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
 var restartButton = document.getElementById("reset");
 var message = document.querySelector("#message");
 var modeBtns = document.querySelectorAll(".mode");
+var container = document.querySelector("#container");
 
 init();
 
@@ -20,11 +22,24 @@ function init() {
 function setupModeBtns() {
     for (var i = 0; i < modeBtns.length; i++) {
         modeBtns[i].addEventListener("click", function() {
-            modeBtns[0].classList.remove("selected");
-            modeBtns[1].classList.remove("selected");
+            for (var j = 0; j < modeBtns.length; j++) {
+                modeBtns[j].classList.remove("selected");
+            }
             this.classList.add("selected");
-
-            this.textContent === "easy" ? numSquares = 3:numSquares = 6;
+            hardMode = false;
+            if (this.textContent === "easy") {
+                container.style.maxWidth = "800px";
+                numSquares = 3;
+            } else if (this.textContent === "medium") {
+                container.style.maxWidth = "600px";
+                numSquares = 6;
+            } else {
+                container.style.maxWidth = "400px";
+                numSquares = 9;
+                if (this.textContent === "expert") {
+                    hardMode = true;
+                }
+            }
             reset();
         });
     }
@@ -34,7 +49,6 @@ function setupSquares() {
     for(var i=0; i < squares.length; i++) {
         squares[i].addEventListener("click", function() {
             var clickedColor = this.style.backgroundColor;
-            console.log(clickedColor + "\n" + pickedColor);
             if (clickedColor === pickedColor) {
                 message.textContent = "Correct!";
                 restartButton.textContent = "Play Again?";
@@ -55,10 +69,20 @@ function setupRestartBtn() {
 
 function generateColors(num) {
     var arr = []
+    var rFloor = 0;
+    var gFloor = 0;
+    var bFloor = 0;
+    var range = 256;
+    if (hardMode == true) {
+        rFloor = Math.random() * 156;
+        gFloor = Math.random() * 156;
+        bFloor = Math.random() * 156;
+        range = 10;
+    }
     for (var i = 0; i < num; i++) {
-        r = Math.floor(Math.random() * 256);
-        g = Math.floor(Math.random() * 256);
-        b = Math.floor(Math.random() * 256);
+        r = Math.floor(rFloor + Math.random() * range);
+        g = Math.floor(gFloor + Math.random() * range);
+        b = Math.floor(bFloor + Math.random() * range);
         arr[i] = "rgb(" + r + ", " + g + ", " + b + ")";
     }
     return arr;
